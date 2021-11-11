5df4938cdfe02fd078a2659b66c6a8de0249ded7
dankarger <karger.dan@gmail.com>
2021-11-11 22:01:54 +0200

    exercise 2.7

---
+++ exercise-2.7.js
@@ -0,0 +1,10 @@
+function basicOp(operator,value1,value2) {
+    if(operator==='+') return value1 + value2;
+    if(operator==='-') return value1 - value2;
+    if(operator==='/') return value1 / value2;
+    if(operator==='*') return value1 * value2;
+}
+
+console.log(basicOp('+', 4, 7));
+console.log(basicOp('-', 15, 18));
+console.log(basicOp('*', 5, 5));
