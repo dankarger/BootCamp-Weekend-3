a58dfc087e97d7dbba0714438a15dc6c03e09b9e
dankarger <karger.dan@gmail.com>
2021-11-12 00:13:46 +0200

    exercise 5.2

---
+++ exercise-5.2.js
@@ -0,0 +1,10 @@
+const repeatStr=(n,str)=>{
+    let result = '';
+    for(let i=1;i<=n;i++) {
+        result+=str;
+    }
+    return result;
+}
+//Test
+console.log(repeatStr(5, "Hello"));
+console.log(repeatStr(4, "i"));
