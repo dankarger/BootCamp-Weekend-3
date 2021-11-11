67ea31b6b72ec4dec4665587aefc59d36da5826d
dankarger <karger.dan@gmail.com>
2021-11-11 23:36:29 +0200

    exercise 4.1

--- exercise-4.1.js
+++ exercise-4.1.js
@@ -1,35 +1,33 @@
 //First option of fibunaci
-function fibonaci(n) {
-    let arr = [1,1];
-    if(n===1||n===2){
-        return 1;
+function fibonaci(n,start) {
+    //if starting point is 1
+    if(start===1) {
+        let arr = [1, 1];
+        if (n === 1 || n === 2) {
+            return 1;
+        }
+        for (let i = 2; i <= n; i++) {
+            arr.push(arr[i - 2] + arr[i - 1]);
+        }
+        return arr[arr.length - 1]
     }
-    for(let i=2;i<=n;i++) {
-        arr.push(arr[i-2]+arr[i-1]);
+    //if starting point is 0
+    if(start===0) {
+        let arr = [0,1,1];
+        if(n===1){
+            return 0;
+        }
+        if(n===2||n===3){
+            return 1;
+        }
+        for(let i=3;i<=n;i++) {
+            arr.push(arr[i-2]+arr[i-1]);
+        }
+        return arr[arr.length-1]
     }
-    return arr[arr.length-1]
 }
 
 //Test
 for(let i=1;i<=12;i++){
-   console.log( fibonaci(i))
-}
-
-//Second option of fibonaci
-function fibonaci2(n) {
-    let arr = [0,1,1];
-    if(n===1){
-        return 0;
-    }
-    if(n===2||n===3){
-        return 1;
-    }
-    for(let i=3;i<=n;i++) {
-        arr.push(arr[i-2]+arr[i-1]);
-    }
-    return arr[arr.length-1]
-}
-//Test
-for(let i=1;i<=12;i++){
-    console.log( fibonaci2(i))
+   console.log( fibonaci(i,1))
 }
