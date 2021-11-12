0f659875e8e5a1e16b22fe93f24e3b2a435535fd
dankarger <karger.dan@gmail.com>
2021-11-12 17:47:22 +0200

    exercise 5.8

--- exercise-5.8.js
+++ exercise-5.8.js
@@ -1,8 +1,8 @@
 function findLongestWord(str) {
     let arr=str.split(' ');
-    let longestWord = arr[0].length;
-    arr.forEach(element=> element.length>longestWord?longestWord=element.length:longestWord)
+    let longestWord = arr[0];
+    arr.forEach(element=> element.length>longestWord.length?longestWord=element:longestWord)
     return longestWord;
 }
 
-console.log(findLongestWord('this is a test'))
+console.log(findLongestWord('this is a test blablabla'))
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 #Weekend 3 - Assignment
 
-*Exercises are part of the "AppleSeed" FullStack BootCamp*
+*"AppleSeed"* FullStack BootCamp- 2021
+
+<a href="https://www.freeiconspng.com/img/34368" title="Image from freeiconspng.com"><img src="https://www.freeiconspng.com/uploads/cat-icon-15.png" width="90" alt="Free High quality Cat Icon" /></a>
